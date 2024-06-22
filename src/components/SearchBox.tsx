c141afd1ca2397972f70f1458f50f3217e7c12af
JuanCarrasquilla360 <juancarrasquilla135219@hotmail.com>
2024-06-21 23:31:59 -0500

    working on header styles

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,28 +1,33 @@
 import React, { useState, FormEvent } from 'react';
+import { CiSearch } from 'react-icons/ci';
+import logo from '../assets/mercadolibre_logo.png'
+import { useNavigate } from 'react-router-dom';
 
 interface SearchBoxProps {
-  onSearch: (query: string) => void;
+    onSearch: (query: string) => void;
 }
 
 const SearchBox: React.FC<SearchBoxProps> = ({ onSearch }) => {
-  const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate()
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSearch(query);
-  };
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onSearch(query);
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="text"
-        placeholder="Buscar productos..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button type="submit">Buscar</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} className='search-field-container'>
+            <img src={logo} alt="" onClick={()=>navigate("/")}/>
+            <input
+                type="text"
+                placeholder="Nunca dejes de buscar"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button type='submit'><CiSearch size={20}/></button>
+        </form>
+    );
 }
 
 export default SearchBox;
